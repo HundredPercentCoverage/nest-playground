e36d18dce8f52455f30dd98b2594a4d812f315cc
HundredPercentCoverage <bunglenutter@gmail.com>
2025-02-16 15:11:09 +0000

    Add some CRUD

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -5,4 +5,20 @@ export class TodoService {
   getAllTodos() {
     return [{ message: 'OK' }];
   }
+
+  getTodoById(id: number) {
+    return { message: 'This is a specific todo of id = ' + id };
+  }
+
+  addTodo() {
+    return { message: 'You have added a todo' };
+  }
+
+  updateTodo() {
+    return { message: 'You have updated a todo' };
+  }
+
+  deleteTodo() {
+    return { message: 'You have deleted a todo' };
+  }
 }
