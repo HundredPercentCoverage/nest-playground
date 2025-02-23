9d6f5dd1577faf2f8bd74bfabfd8c74f893b08c7
HundredPercentCoverage <bunglenutter@gmail.com>
2025-02-23 14:01:07 +0000

    Update endpoints

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,17 +1,34 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Controller('todo')
 export class TodoController {
-  constructor(private authService: TodoService) {}
+  constructor(private todoService: TodoService) {}
 
   @Get()
   getTodos() {
-    return this.authService.getAllTodos();
+    return this.todoService.getAllTodos();
   }
 
   @Get(':id')
   getTodo(@Param('id', ParseIntPipe) id: number) {
-    return this.authService.getTodoById(id);
+    return this.todoService.getTodoById(id);
+  }
+
+  @Post()
+  createTodo(@Body() createTodoDto: CreateTodoDto) {
+    return this.todoService.addTodo(createTodoDto);
+  }
+
+  @Put(':id')
+  updateTodo(@Body() updateTodoDto: UpdateTodoDto, @Param('id', ParseIntPipe) id: number) {
+    return this.todoService.updateTodo(id, updateTodoDto);
+  }
+
+  @Delete(':id')
+  deleteTodo(@Param('id', ParseIntPipe) id: number) {
+    return this.todoService.deleteTodo(id);
   }
 }
