29e58090e82106d77bc17e5df12511bc344ba8f3
HundredPercentCoverage <bunglenutter@gmail.com>
2025-02-26 16:23:52 +0000

    Comments

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,5 +1,8 @@
 import { IsNotEmpty, IsString } from "class-validator";
 
+// DTO defines the shape of the data to be sent to the service
+// As such it can be used to validate the shape of any input data
+// in requests to the endpoint
 export class CreateTodoDto {
   @IsNotEmpty({ message: 'Todo field must not be empty' })
   @IsString()
